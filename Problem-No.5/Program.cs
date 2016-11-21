0f5d97d3aac29e9c8c40ac3608383cff88125e1b
IvMollov <mollov.i@abv.bg>
2016-11-21 23:53:16 +0200

    Add files via upload

---
+++ Problem-No.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    public class MultiplicationTable
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a digit: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 11; i++)
+            {
+                int result = number * i;
+                Console.WriteLine(number + " x " + i + " = " + result);
+            }
+        }
+    }
+
+}
