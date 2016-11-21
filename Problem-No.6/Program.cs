58f77e1d5592b8924d21b0b33d22d475486c1da9
IvMollov <mollov.i@abv.bg>
2016-11-21 23:55:02 +0200

    Add files via upload

---
+++ Problem-No.6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print
+{
+ public class Mystery
+{ 
+   public static void Main(string[] args)
+        { 
+     int count = 1; 
+     while ( count <= 10 ) 
+     { 
+       Console.WriteLine( count % 2 == 1 ? "****" : "++++++++" ); 
+        count++; 
+      } 
+  } 
+ } 
+
+}
