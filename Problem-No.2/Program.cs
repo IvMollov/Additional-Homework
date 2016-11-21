5a313a66a7e85f01fd46e8192c74dc626bd187e6
IvMollov <mollov.i@abv.bg>
2016-11-21 23:44:16 +0200

    Add files via upload

---
+++ Problem-No.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayNumbers
+{
+    public class DisplayNumbers
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a digit \"a\": ");
+            int numberA = int.Parse(Console.ReadLine());
+            Console.Write("Enter a digit \"b\": ");
+            int numberB = int.Parse(Console.ReadLine());
+            Console.Write("Enter a digit \"c\": ");
+            int numberC = int.Parse(Console.ReadLine());
+
+            int resultOne = (numberA + numberB) * numberC;
+            Console.WriteLine("(a+b)*c = {0}", resultOne);
+
+            int resultTwo = numberA * numberC + numberB * numberC;
+            Console.WriteLine("a*c + b*c = {0}", resultTwo);
+        }
+    }
+
+}
