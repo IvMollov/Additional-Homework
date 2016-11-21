6d6d51e9596c593fa48d30ea9829daf5fb5bd0ab
IvMollov <mollov.i@abv.bg>
2016-11-22 00:15:34 +0200

    Add files via upload

---
+++ Problem-No.9a/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonDevisor
+{
+    class Program
+    {
+        public static bool CommonDevisor(int x, int y, out int least, out int greatest)
+        {
+            int max = x < y ? x : y;
+            bool first = true;
+            least = 1;
+            greatest = 1;
+            for(int i = 2; i <= max/2 + 1; i++)
+            {
+                if(((x % i) == 0) && ((y % i) == 0))
+                {
+                    if(first)
+                    {
+                        least = i;
+                        first = false;
+                    }
+                    greatest = i;
+                }
+            }
+            if (least != 1) return true;
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter y: ");
+            int lcd, gcd;
+            int y = int.Parse(Console.ReadLine());
+            if(CommonDevisor(x, y, out lcd, out gcd ))
+            {
+                Console.WriteLine("Least common devisor of {0} and {1} is {2}", x, y, lcd);
+                Console.WriteLine("Greatest common devisor of {0} and {1} is {2}", x, y, gcd);
+            }
+            else
+            {
+                Console.WriteLine("No common devisor of {0} and {1}", x, y);
+            }
+        }
+    }
+}
