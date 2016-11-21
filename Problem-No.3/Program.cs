7062e906ab38030af52a9bd8e059cb2bd481f78d
IvMollov <mollov.i@abv.bg>
2016-11-21 23:47:10 +0200

    Add files via upload

---
+++ Problem-No.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayRectangle
+{
+    public class DisplayRectangle
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a digit: ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+
+                    if (i > 0 && i < 4)
+
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write(number);
+                        }
+
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.Write(number);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+
+}
